a2e6d5b0d9506010d2824ed093ec1b5ef2c71a80
Quinntyne Brown <qbrown@cetaris.com>
2015-10-18 14:44:33 -0400

    feat(photography): add compile to carousel

--- src/app/ui/components/carousel/carousel.directive.ts
+++ src/app/ui/components/carousel/carousel.directive.ts
@@ -2,9 +2,16 @@
  
 module App.UI {
 
+    /**
+     * @name Carousel
+     */
     export class Carousel {
 
-        public static createInstance = () => { }
+        constructor(private $compile: ng.ICompileService) { }
+
+        public static createInstance = ($compile: ng.ICompileService) => {
+            return new Carousel($compile);
+        }
 
         public templateUrl: string = "src/app/ui/components/carousel/carousel.html";
 
@@ -16,10 +23,39 @@ module App.UI {
 
         public controller: string = "carouselController";
 
+        public scope:any = {
+            slideTemplateUrl: "@",                        
+        }
+
         public link = (scope: ng.IScope, element: ng.IAugmentedJQuery, attributes: ng.IAttributes) => {
 
+
+            var items: Array<any> = parseItems(scope, attributes);
+
+            for (var i = 0; i < items.length; i++) {
+                
+            }
+
+            function parseItemName(attributes: ng.IAttributes): string {
+                var match = attributes["carousel"].match(/^\s*(.+)\s+in\s+(.*?)\s*(\s+track\s+by\s+(.+)\s*)?$/);
+                return match[1];
+            }
+
+            function parseItems(scope: ng.IScope, attributes: ng.IAttributes): Array<any> {
+                var match = attributes["carousel"].match(/^\s*(.+)\s+in\s+(.*?)\s*(\s+track\s+by\s+(.+)\s*)?$/);
+                if (match) {
+                    var collectionStringArray = match[2].split(".");
+                    var items: any = scope;
+                    for (var i = 0; i < collectionStringArray.length; i++) {
+                        items = items[collectionStringArray[i]];
+                    }
+                    return items;
+                } else {
+                    return JSON.parse(attributes["carousel"]);
+                }
+            }
         }
     }
 
-    angular.module("app.ui").directive("carousel", [Carousel.createInstance]);
+    angular.module("app.ui").directive("carousel", ["$compile",Carousel.createInstance]);
 }
