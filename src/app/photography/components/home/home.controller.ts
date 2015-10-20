bf8c069d0722853be2e2f75dc7421499675dfd85
Quinntyne Brown <qbrown@cetaris.com>
2015-10-19 21:27:43 -0400

    feat(home): canActivate func

--- src/app/photography/components/home/home.controller.ts
+++ src/app/photography/components/home/home.controller.ts
@@ -1,7 +1,13 @@
 ﻿module App.Photography {
     
-    export class HomeController {
+    export class HomeController implements IHomeController {
         constructor(routeData: any, photo: IPhoto) { }
+
+        private _slideTemplate: string;
+
+        public get slideTemplate() { return this._slideTemplate; }
+
+        public set slideTemplate(value:string) { this._slideTemplate = value; }
 
         private _photos: Array<IPhoto> = [];
 
@@ -10,9 +16,23 @@
         public set photos(value: Array<IPhoto>) { this._photos = value; }
 
         public static canActivate = () => {
-            return ["$http", "$q", "photographyDataService", ($http: ng.IHttpService, $q: ng.IQService, photographyDataService: any) => {
-   
-                }];
+            return ["$http", "$q", "photographyDataService", ($http: ng.IHttpService, $q: ng.IQService, photographyDataService: IPhotographyDataService) => {
+
+                var deferred = $q.defer();
+
+                $q.all([
+                    $http.get("src/app/photography/views/partials/slideTemplate.html"),
+                    photographyDataService.getAllFeaturedPhotos()
+                ]).then((results: any) => {
+                    deferred.resolve({
+                        slideTemplate: results[0],
+                        photos: results[1]
+                    });
+                });
+
+                return deferred.promise;
+
+            }];
         }
     }
 
--- src/app/photography/photography.d.ts
+++ src/app/photography/photography.d.ts
@@ -3,4 +3,13 @@
     export interface IPhoto {
         
     }
+
+    export interface IHomeController {
+        slideTemplate: string;
+        photos:Array<IPhoto>;
+    }
+
+    export interface IPhotographyDataService {
+        getAllFeaturedPhotos():ng.IPromise<any>;
+    }
 } 
