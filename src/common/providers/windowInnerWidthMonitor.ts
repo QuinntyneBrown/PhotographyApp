9988fe8ee1c7389f2f8d1b47e41f7be375bd67f3
Quinntyne Brown <qbrown@cetaris.com>
2015-10-27 19:36:53 -0400

    feat(app.common): add debounce

--- src/common/providers/windowInnerWidthMonitor.ts
+++ src/common/providers/windowInnerWidthMonitor.ts
@@ -4,9 +4,12 @@
 
     export class WindowInnerWidthMonitor {
         
-        constructor(private $rootScope:ng.IRootScopeService) {
-            window.addEventListener("resize", onResize);
+        constructor(private $rootScope: ng.IRootScopeService, private debounce: any) {
+
+            window.addEventListener("resize", debounce(onResize,300));
+
             var self = this;
+
             function onResize() {
                 if (self.innerWidth != window.innerWidth) {
                     self.innerWidth = window.innerWidth;
@@ -20,7 +23,7 @@
     }
 
     angular.module("app.common")
-        .service("windowInnerWidthMonitor", ["$rootScope", WindowInnerWidthMonitor])
+        .service("windowInnerWidthMonitor", ["$rootScope","debounce", WindowInnerWidthMonitor])
         .run(["windowInnerWidthMonitor", (windowInnerWidthMonitor: any) => {}]);
 
 } 
