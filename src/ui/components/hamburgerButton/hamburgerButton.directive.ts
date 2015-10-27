e18e134e30c5bd5d1ef73240646e8e5522461d51
Quinntyne Brown <qbrown@cetaris.com>
2015-10-27 17:28:48 -0400

    feat(app.ui): add hamburger button component

---
+++ src/ui/components/hamburgerButton/hamburgerButton.directive.ts
@@ -0,0 +1,32 @@
+﻿/// <reference path="../../../../typings/typescriptapp.d.ts" />
+
+module App.UI {
+
+    "use strict";
+
+    /**
+     * @name HamburgerButton
+     * @module App.UI
+     */
+    export class HamburgerButton {
+
+        public static createInstance = () => { return new HamburgerButton(); }
+
+        public template: string = [
+            "<div class='hamburger-button' data-ng-click='onClick()'>",
+            "<div></div>",
+            "<div></div>",
+            "<div></div>",
+            "</div>"
+        ].join(" ");
+
+        public replace: boolean = true;
+
+        public restrict: string = "E";
+
+        public scope: any = { onClick: "&" };
+    }
+
+    angular.module("app.ui").directive("hamburgerButton", [HamburgerButton.createInstance]);
+
+} 
