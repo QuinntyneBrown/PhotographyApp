f9a5bb4593b3e49fec964916e40cb9c4c35531e5
Quinntyne Brown <qbrown@cetaris.com>
2015-10-27 22:39:08 -0400

    feat(app.common): add parseBoolean

---
+++ src/common/providers/parseBoolean.ts
@@ -0,0 +1,16 @@
+﻿module App.Common {
+
+    "use strict";
+
+    /**
+     * @name parseBoolean
+     * @module App.Common
+     * @description convert string true or false to boolean value
+     */
+    export var parseBoolean = (options: any) => {
+        return options.valueOf && options.value.toUpperCase() === "TRUE";
+    }
+
+    angular.module("app.common").value("parseBoolean", parseBoolean);
+
+} 
