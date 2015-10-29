20a84c9259c161960caa7e8b87d283ec7f3deb1c
Quinntyne Brown <qbrown@cetaris.com>
2015-10-29 01:22:19 -0400

    feat(app.common): add compileAsync

---
+++ src/common/providers/compileAsync.ts
@@ -0,0 +1,20 @@
+﻿module App.Common {
+
+    "use strict";
+
+    var $q: ng.IQService = <ng.IQService>angular.injector(['ng']).get("$q");
+    var $compile: ng.ICompileService = <ng.ICompileService>angular.injector(['ng']).get("$compile");
+
+    /**
+     * @name compileAsync
+     * @module App.Common
+     */
+    export var compileAsync = (options: any) => {
+        var deferred = $q.defer();        
+        options.augmentedJQuery = $compile(options.template)(options.scope);
+        deferred.resolve();
+        return deferred.promise;
+    }
+
+    angular.module("app.common").value("compileAsync", compileAsync);
+} 
---
+++ src/ui/components/flyout/flyout.ts
@@ -0,0 +1,70 @@
+﻿module App.UI {
+
+    "use strict";
+
+    /**
+     * @name Flyout
+     * @description
+     */
+    export class Flyout {
+        constructor(private $q: ng.IQService, private extendCssAsync:any) { }
+
+        public createInstance = (options:any) => {
+            var instance = new Flyout(this.$q, this.extendCssAsync);
+
+            return instance;
+        }
+
+        public initializeAsync = () => {
+            var deferred = this.$q.defer();
+
+            this.augmentedJQuery = angular.element("<div></div>");
+
+            this.extendCssAsync({
+                nativeHTMLElement: this.nativeElement,
+                cssObject: {
+                    "transition": "transform .500s cubic-bezier(.10, .10, .25, .90)",
+                    "opacity": "100",
+                    "position": "fixed",
+                    "top": "0", // below clicked triggered element
+                    "left": "-100", //negative width
+                    "height": "100%", //calc 100% - top
+                    "width": "100%", // width
+                    "background-color": "rgba(0, 0, 0, .25)", //colour configuraable
+                    "display": "block"
+                }
+            }).then(() => {
+                deferred.resolve();
+            });
+
+            return deferred.promise;
+        }
+
+        public showAsync = () => {
+
+        }
+
+        public hideAsync = () => {
+
+        }
+
+        public dispose = () => {
+
+        }
+
+        public onClick = () => {
+
+        }
+
+        public isOpen: boolean = false;
+
+        public isAnimating: boolean = false;
+
+        private augmentedJQuery: ng.IAugmentedJQuery;
+
+        private get nativeElement(): HTMLElement { return this.augmentedJQuery[0]}
+
+    }
+
+    angular.module("app.ui").service("flyout", ["$q",Flyout]);
+} 
